a96f3cc2fabcb84d008a2484e9b3ee3162925678
Jon Durbin <jon@jondurbin.com>
2023-06-23 05:38:18 -0400

    relative import issue

--- flash_qlora.py
+++ flash_qlora.py
@@ -1,8 +1,8 @@
-from .flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
+from flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 
 replace_llama_attn_with_flash_attn()
 
-from .qlora import train
+from qlora import train
 
 if __name__ == "__main__":
     train()
