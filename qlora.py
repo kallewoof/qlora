74f77a15c0857b85c4d439192667afc6f37fa49d
Jon Durbin <jon@jondurbin.com>
2023-06-23 05:43:17 -0400

    Support triton attention for mpt tuning.

--- qlora.py
+++ qlora.py
@@ -248,14 +248,13 @@ def get_accelerate_model(args, checkpoint_dir):
 
     print(f'loading base model {args.model_name_or_path}...')
     compute_dtype = (torch.float16 if args.fp16 else (torch.bfloat16 if args.bf16 else torch.float32))
-    model = AutoModelForCausalLM.from_pretrained(
-        args.model_name_or_path,
-        cache_dir=args.cache_dir,
-        load_in_4bit=args.bits == 4,
-        load_in_8bit=args.bits == 8,
-        device_map=device_map,
-        max_memory=max_memory,
-        quantization_config=BitsAndBytesConfig(
+    model_kwargs = {
+        "cache_dir": args.cache_dir,
+        "load_in_4bit": args.bits == 4,
+        "load_in_8bit": args.bits == 8,
+        "device_map": device_map,
+        "max_memory": max_memory,
+        "quantization_config": BitsAndBytesConfig(
             load_in_4bit=args.bits == 4,
             load_in_8bit=args.bits == 8,
             llm_int8_threshold=6.0,
@@ -264,10 +263,13 @@ def get_accelerate_model(args, checkpoint_dir):
             bnb_4bit_use_double_quant=args.double_quant,
             bnb_4bit_quant_type=args.quant_type,
         ),
-        torch_dtype=(torch.float32 if args.fp16 else (torch.bfloat16 if args.bf16 else torch.float32)),
-        trust_remote_code=args.trust_remote_code,
-        use_auth_token=args.use_auth_token
-    )
+        "torch_dtype": (torch.float32 if args.fp16 else (torch.bfloat16 if args.bf16 else torch.float32)),
+        "trust_remote_code": args.trust_remote_code,
+        "use_auth_token": args.use_auth_token
+    }
+    if args.mpt:
+        model_kwargs["attn_config"] = {"attn_impl": "triton"}
+    model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, **model_kwargs)
     if compute_dtype == torch.float16 and args.bits == 4:
         major, minor = torch.cuda.get_device_capability()
         if major >= 8:
