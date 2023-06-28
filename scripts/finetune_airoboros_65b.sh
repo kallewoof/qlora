760b25fb02f8b9eb5f56decc30c01fb7d10300db
Jon Durbin <jon@jondurbin.com>
2023-06-28 08:22:52 -0400

    Update finetune_airoboros_65b.sh

--- scripts/finetune_airoboros_65b.sh
+++ scripts/finetune_airoboros_65b.sh
@@ -1,25 +1,29 @@
 export WANDB_API_KEY=[redacted]
 export WANDB_PROJECT=airoboros-65b-gpt4-qlora
 
-# git clone https://hf.co/decapoda-research/llama-65b-hf
-wget -O instructions.jsonl https://huggingface.co/datasets/jondurbin/airoboros-gpt4-1.1/resolve/main/instructions.jsonl
-wget -O /workspace/llama-65b-hf/tokenizer_config.json https://huggingface.co/jondurbin/airoboros-7b-gpt4/resolve/main/tokenizer_config.json
-wget -O /workspace/llama-65b-hf/special_tokens_map.json https://huggingface.co/jondurbin/airoboros-7b-gpt4/resolve/main/special_tokens_map.json
+# Requirements:
+# 1. Get the base model:
+#    git clone https://hf.co/decapoda-research/llama-65b-hf
+# 2. Get the dataset:
+#    wget -O instructions.jsonl https://huggingface.co/datasets/jondurbin/airoboros-gpt4-1.4.1/resolve/main/instructions.jsonl
+# 3. Replace tokenizer_config.json and special_tokens_map.json with corrected versions.
+#    cp special_tokens_map.json /workspace/llama-65b-hf/
+#    cp tokenizer_config.json /workspace/llama-65b-hf/
 
+# This can optionally be python flash_qlora.py
 python qlora.py \
     --model_name_or_path /workspace/llama-65b-hf \
-    --output_dir /workspace/airoboros-65b-gpt4-qlora \
-    --max_steps 1000 \
+    --output_dir /workspace/$WANDB_PROJECT-checkpoints \
+    --num_train_epochs 3 \
     --logging_steps 1 \
     --save_strategy steps \
-    --data_seed 73771 \
-    --save_steps 25 \
-    --save_total_limit 5 \
+    --data_seed 11422 \
+    --save_steps 500 \
+    --save_total_limit 10 \
     --evaluation_strategy "no" \
     --eval_dataset_size 2 \
-    --max_new_tokens 2048 \
+    --max_new_tokens 1800 \
     --dataloader_num_workers 3 \
-    --group_by_length \
     --logging_strategy steps \
     --remove_unused_columns False \
     --do_train \
@@ -32,17 +36,16 @@ python qlora.py \
     --bits 4 \
     --warmup_ratio 0.03 \
     --lr_scheduler_type constant \
-    --gradient_checkpointing \
-    --dataset instructions.jsonl \
+    --dataset /workspace/instructions.jsonl \
     --dataset_format airoboros \
-    --source_max_len 2048 \
-    --target_max_len 2048 \
+    --model_max_len 2048 \
     --per_device_train_batch_size 2 \
-    --gradient_accumulation_steps 16 \
     --learning_rate 0.0001 \
     --adam_beta2 0.999 \
     --max_grad_norm 0.3 \
     --lora_dropout 0.05 \
     --weight_decay 0.0 \
-    --seed 73771 \
-    --report_to wandb
+    --seed 11422 \
+    --report_to wandb \
+    --gradient_accumulation_steps 16 \
+    --gradient_checkpointing
