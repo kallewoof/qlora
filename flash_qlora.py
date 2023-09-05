b63edc5bb5acee758f277cf6fc68be6ccc2a79dd
Jon Durbin <jon@jondurbin.com>
2023-09-05 03:50:45 -0400

    Update flash_qlora.py

--- flash_qlora.py
+++ flash_qlora.py
@@ -1,6 +1,6 @@
-from flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
+from flash_attn_monkey_patch import replace_attn_with_flash_attn
 
-replace_llama_attn_with_flash_attn()
+replace_attn_with_flash_attn()
 
 from qlora import train
 
